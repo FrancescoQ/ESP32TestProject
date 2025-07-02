8a02a8bedd2a41f3311a958697b25faef0dad52d
Francesco Quagliati <francesco.quagliati@gmail.com>
2025-07-02 08:56:00 +0200

    tmp

--- include/Sensor.h
+++ include/Sensor.h
@@ -22,54 +22,55 @@ enum class SensorType
  */
 struct Sensor
 {
-  /**
-   * The sensor type
-   */
-  SensorType type;
-  /**
-   * @brief The pin where the sensor is connected to the board.
-   */
-  int pin;
+  public:
+    /**
+     * The sensor type
+     */
+    SensorType type;
+    /**
+     * @brief The pin where the sensor is connected to the board.
+     */
+    int pin;
 
-  /**
-   * @brief The current value.
-   * The value can be updated by the update() method below, this is the
-   * property with the stored value.
-   */
-  int value;
+    /**
+     * @brief The current value.
+     * The value can be updated by the update() method below, this is the
+     * property with the stored value.
+     */
+    int value;
 
-  /**
-   * @brief The sensor label as passed when the sensor is added.
-   */
-  String label;
+    /**
+     * @brief The sensor label as passed when the sensor is added.
+     */
+    String label;
 
-  /**
-   * @brief Constructor to build the sensor.
-   * The value is always initialized to 0
-   * @param SensorType t
-   * @param int p
-   * @param String l
-   */
-  Sensor(SensorType t, int p, const String &l) : type(t), pin(p), label(l), value(0) {}
+    /**
+     * @brief Constructor to build the sensor.
+     * The value is always initialized to 0
+     * @param SensorType t
+     * @param int p
+     * @param String l
+     */
+    Sensor(SensorType t, int p, const String &l) : type(t), pin(p), label(l), value(0) {}
 
-  /**
-   * @brief The update method to get the new value to be stored.
-   */
-  void update()
-  {
-    switch (type)
-    {
-    case SensorType::Movement:
-      // value = digitalRead(pin);
-      value = random(0, 2); // Fake movement
-      break;
-    case SensorType::Light:
-      // value = analogRead(pin);
-      value = random(0, 1024); // Fake light
-      break;
-    case SensorType::Fake:
-      value = random(0, 100);
-      break;
-    }
-  }
+    /**
+     * @brief The update method to get the new value to be stored.
+     */
+    virtual void update() = 0;
+  // {
+  //   switch (type)
+  //   {
+  //   case SensorType::Movement:
+  //     // value = digitalRead(pin);
+  //     value = random(0, 2); // Fake movement
+  //     break;
+  //   case SensorType::Light:
+  //     // value = analogRead(pin);
+  //     value = random(0, 1024); // Fake light
+  //     break;
+  //   case SensorType::Fake:
+  //     value = random(0, 100);
+  //     break;
+  //   }
+  // }
 };
